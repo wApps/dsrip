c28362077786aef136f7db43ca6ad6120075f0c3
Jonas Almeida <jalmeida@mathbiol.org>
2015-06-05 15:54:50 -0400

    login

---
+++ dsrip.js
@@ -0,0 +1,51 @@
+console.log('dsrip.js loaded')
+
+dsrip={}
+dsrip.buildSBU=function(id){
+    console.log('assembling DSRIP UI')
+    var div = document.getElementById(id)
+    div.innerHTML=""
+    var divLog = document.createElement('div')
+    divLog.innerHTML='First we need to make sure you are authorized to connect to CRM database.<h3>Basic authentication credentials:</h3><form autocomplete="on" onsubmit="return false">Username <input id="username" type="text"><br>Password <input id="password" type="password"><br><input id="submitBasicCredentials" type="Submit" value="Login">  <span id="submitBasicCredentialsMsg"></span></form>'
+    divLog.style.color="navy"
+    div.appendChild(divLog)
+    submitBasicCredentials.onclick=function(ev){
+        //console.log(ev)
+        submitBasicCredentialsMsg.textContent=" connecting ..."
+        submitBasicCredentialsMsg.style.color="red"
+        var username = this.parentElement.children[0].value
+        var password = this.parentElement.children[2].value
+        //dsrip.loginSBU
+        dsrip.callSBU=function(url,fun,err){
+            if(!fun){fun=function(x){console.log(x)}}
+            if(!err){err=function(x){console.log(x)}}
+            // lock sensitive login information in the scope of the calling function
+            var login={username:username,password:password}
+            $.ajax({
+                type: "GET",
+                url: url,
+                dataType: 'json',
+                //username: login.username,
+                //password: login.password,
+                headers:{"Authorization": "Basic " + btoa(login.username + ":" + login.password)},
+                success: fun,
+                error:err
+            });
+        }
+        // make sure connection was successfull
+        dsrip.callSBU(
+            "https://bmi-clinical-apps.uhmc.sunysb.edu/limited_soda/sparcs_lds_ip.suffolk_sparcs_ip_with_pqi_pdi_indicator_2?$limit=0",
+            function(){ // on success
+                submitBasicCredentialsMsg.textContent=" connection succeeded"
+                submitBasicCredentialsMsg.style.color="green"
+                setTimeout(function(){
+                    divLog.innerHTML='<j4 style="color:green">You are logged in BMI data infrastructure</h4> <button id="logOutSBU" style="color:maroon">log out</button>'
+                    document.getElementById("logOutSBU").onclick=function(){dsrip.buildSBU(id)}
+                },2000)
+            },
+            function(){ // on error
+                submitBasicCredentialsMsg.textContent=" connection failed :-("
+            })
+        4
+    }
+}
---
+++ manifest.js
@@ -0,0 +1,64 @@
+// This document, manifest.js, is the only file a wApps repository needs to maintain.
+// it includes four sections: 1) Branding, 2) Tabs, 3) Apps, and 4) Authors
+
+
+// 1) BRANDING - the icon and link in the upper left corner
+wApps.manifest.brand={
+    pic:'brandMathBiol.png',
+    url:'https://github.com/wApps/manifest#wapps-all-you-need-is-a-manifest'
+};
+
+// 2) TABS - the navigation tabs in the head of wApps. 
+//    The code manage "myApps", "Store" and "People",
+//    the rest is up to you.
+wApps.manifest.tabs={
+    "myApps":{
+        html:'Apps you selected from the AppStore ...',
+        Div:{} // where the DOM element will be set later 
+    },
+    "Store":{
+        html:'Retrieving list of Apps from the manifest ...',
+        Div:{}
+    },
+    "People":{
+        html:'Retrieving list of people authoring Apps ...',
+        Div:{}
+    },
+    "About":{
+        html:'<h1>wApps</h1>This is an experiment in loosening the architecture of a webApp store to achieve a deeper integration between autonomously developed components.',
+        Div:{}
+    }
+};
+
+// 3) APPS - the description of the applications 
+wApps.manifest.apps.push(
+
+    {
+    "name" : 'Login DSRIP',
+    "description" : "Generic app to login DSRIP's CRM.",
+    "url" : 'https://github.com/wApps/dsrip',
+    "author" : 'Jonas Almeida',
+    buildUI : function(id){ 
+        this.require('http://localhost:8000/wapps_dsrip/dsrip.js', 
+            function () {
+                dsrip.buildSBU(id);
+            }
+        )}
+    }
+);
+
+// 4) AUTHORS - description of the authors, matching the names in the Apps,
+//              where they can be described as a string or, when there is
+//              a team of authors, as an Array of strings .
+wApps.manifest.authors.push(
+    {
+    "name":"Jonas Almeida",
+    "url":"http://jonasalmeida.info"
+    },
+    
+    {
+    "name":"Some Author",
+    "url":"http://someUrl.com"
+    }
+);
+
