6993c5c9781d51cbc0bbc97e0cce353f9500fd84
Jonas Almeida <jalmeida@mathbiol.org>
2015-06-22 14:01:20 -0400

    localhost pollution cleaned

--- manifest.js
+++ manifest.js
@@ -39,7 +39,7 @@ wApps.manifest.apps.push(
     "url" : 'https://github.com/wApps/dsrip',
     "author" : 'Jonas Almeida',
     buildUI : function(id){ 
-        this.require(['https://cdnjs.cloudflare.com/ajax/libs/localforage/1.2.2/localforage.js','http://localhost:8000/wapps_dsrip/dsrip.js'],
+        this.require(['https://cdnjs.cloudflare.com/ajax/libs/localforage/1.2.2/localforage.js', 'https://wapps.github.io/dsrip/dsrip.js'],// 'http://localhost:8000/wapps_dsrip/dsrip.js'],
         //this.require('https://wapps.github.io/dsrip/dsrip.js', 
             function () {
                 dsrip.buildSBU(id);
