11ec242a952432864af03fca7b8c8234b16ec079
Jonas Almeida <jalmeida@mathbiol.org>
2015-06-30 14:59:49 -0400

    first graphs

--- dsrip.js
+++ dsrip.js
@@ -93,6 +93,42 @@ dsrip.SBU_sparcs.loadData=function(fun){
         localforage.getItem(
             "dsrip.SBU_sparcs.tab",
             function(err,x){
+                var callSBU=function(){
+                    dsrip.callSBU( // (url,fun,err,cache)
+                        url+"?$limit="+n+"&$offset="+i,//dsrip.SBU_sparcs.x.length,
+                        function(x){
+                            i+=x.length
+                            //dsrip.SBU_sparcs.x=dsrip.SBU_sparcs.x.concat(x)
+                            var msgi= new Date()+': loaded '+i+' reccords ...'
+                            dsrip.pushDocs2Tab(x,dsrip.SBU_sparcs.tab)
+                            //console.log(new Date()+': loaded '+dsrip.SBU_sparcs.x.length+' reccords ...')
+                            loadingStatus.innerHTML+='\n'+msgi
+                            if((x.length==n)){//&(i<20000)){
+                                loadData()
+                            }else{
+                                console.log("done with dsrip.SBU_sparcs.loadData")
+                                loadingStatus.innerHTML+='\n'+'... a total of '+i+' reccords were loaded ...'
+                                if(dsrip.callSBU.offlineData){
+                                    loadingStatus.innerHTML+='\n'+'... your offline setting allows for data caching, doing it ...'
+                                    localforage.setItem(
+                                        "dsrip.SBU_sparcs.tab",
+                                        dsrip.SBU_sparcs.tab,
+                                        function(err){
+                                            if(err){
+                                                loadingStatus.innerHTML+='\n'+'... something went wrong while trying to cache data :-('
+                                            }else{
+                                                loadingStatus.innerHTML+='\n'+'... '+dsrip.SBU_sparcs.tab[Object.getOwnPropertyNames(dsrip.SBU_sparcs.tab)[0]].length+' reccords cached'
+                                            }
+                                        } 
+                                    )
+
+                                }
+
+                                fun()
+                            }
+                        }
+                    )
+                }
                 if(err){
                     loadingStatus.innerHTML+='\n'+'... something went wrong while loading data from cache :-('
                 }else {
@@ -101,40 +137,7 @@ dsrip.SBU_sparcs.loadData=function(fun){
                         loadingStatus.innerHTML+='\n'+'... '+dsrip.SBU_sparcs.tab[Object.getOwnPropertyNames(dsrip.SBU_sparcs.tab)[0]].length+' reccords loaded from cache'
                         fun()
                     }else{
-                        dsrip.callSBU( // (url,fun,err,cache)
-                            url+"?$limit="+n+"&$offset="+i,//dsrip.SBU_sparcs.x.length,
-                            function(x){
-                                i+=x.length
-                                //dsrip.SBU_sparcs.x=dsrip.SBU_sparcs.x.concat(x)
-                                var msgi= new Date()+': loaded '+i+' reccords ...'
-                                dsrip.pushDocs2Tab(x,dsrip.SBU_sparcs.tab)
-                                //console.log(new Date()+': loaded '+dsrip.SBU_sparcs.x.length+' reccords ...')
-                                loadingStatus.innerHTML+='\n'+msgi
-                                if((x.length==n)){//&(i<20000)){
-                                    loadData()
-                                }else{
-                                    console.log("done with dsrip.SBU_sparcs.loadData")
-                                    loadingStatus.innerHTML+='\n'+'... a total of '+i+' reccords were loaded ...'
-                                    if(dsrip.callSBU.offlineData){
-                                        loadingStatus.innerHTML+='\n'+'... your offline setting allows for data caching, doing it ...'
-                                        localforage.setItem(
-                                            "dsrip.SBU_sparcs.tab",
-                                            dsrip.SBU_sparcs.tab,
-                                            function(err){
-                                                if(err){
-                                                    loadingStatus.innerHTML+='\n'+'... something went wrong while trying to cache data :-('
-                                                }else{
-                                                    loadingStatus.innerHTML+='\n'+'... '+dsrip.SBU_sparcs.tab[Object.getOwnPropertyNames(dsrip.SBU_sparcs.tab)[0]].length+' reccords cached'
-                                                }
-                                            }
-                                        )
-
-                                    }
-
-                                    fun()
-                                }
-                            }
-                        )
+                        callSBU()
                     }
                 }            
             }
@@ -147,8 +150,12 @@ dsrip.SBU_sparcs.loadData=function(fun){
 dsrip.SBU_sparcs.buildUI=function(id){
     console.log('assembling SBU SPARCS UI ...')
     var div = document.getElementById(id)
-    if(dsrip.callSBU_loggedin){
-        div.innerHTML='<p id="SPARCS_msg">Logged in since '+dsrip.callSBU_loggedin+'</p><div id="SPARCS_UI"></div>'
+    if(dsrip.callSBU_loggedin||dsrip.SBU_sparcs.docs){
+        if(dsrip.callSBU_loggedin){
+           div.innerHTML='<p id="SPARCS_msg">Logged in since '+dsrip.callSBU_loggedin+'</p><div id="SPARCS_UI"></div>'  
+        }else{
+           div.innerHTML='<p id="SPARCS_msg" style="color:red"> Not logged in </p><div id="SPARCS_UI"></div>' 
+        }
         SPARCS_UI.innerHTML='<div id="SPARCS_work">Importing patient data to explore distribution of:<li>Age</li><li>Gender</li><li>Race</li><li>Zip Code</li><li>Payer</li><li>Discharge</li><li>Facility</li></div><pre id="loadingStatus">loading data ...</pre>'
         dsrip.SBU_sparcs.loadData(function(){
             // UI assembled here
@@ -158,15 +165,20 @@ dsrip.SBU_sparcs.buildUI=function(id){
             dsrip.SBU_sparcs.n=dsrip.SBU_sparcs.tab[dsrip.SBU_sparcs.parms[0]].length
             dsrip.SBU_sparcs.docs=dsrip.SBU_sparcs.tab[Object.getOwnPropertyNames(dsrip.SBU_sparcs.tab)[0]].map(function(d,i){
                 var doc={}
-                dsrip.SBU_sparcs.parms.map(function(p,i){
+                dsrip.SBU_sparcs.parms.map(function(p,j){
                     doc[p]=dsrip.SBU_sparcs.tab[p][i]
                 })
                 return doc
             })
             loadingStatus.textContent='Parameter values extracted from '+dsrip.SBU_sparcs.n+' reccords:\n> '+dsrip.SBU_sparcs.parms.join('\n> ')
-            4
-
-
+            var doSPARC=document.createElement('input')
+            div.appendChild(doSPARC)
+            doSPARC.type="button"
+            doSPARC.style.color="blue"
+            doSPARC.value="Analyse"
+            doSPARC.onclick=function(){
+                 dsrip.doSPARC(id)
+            }
         })
     } else {
         div.innerHTML='Note: if you start SPARCS analysis before logging in,<br>an attempt will be made to find cached data.<br> If none is found, you will be coming come back to this page<br> and you really need to log at "Login DSRIP" first.<p> <input id="startSPARCS" type="button" value="Start"></p>'
@@ -174,6 +186,12 @@ dsrip.SBU_sparcs.buildUI=function(id){
             div.innerHTML='<span style="color:red">if you are logged in the analysis will start next, otherwiese you will be taken back to the start button </span>'
             setTimeout(function(){dsrip.SBU_sparcs.buildUI(id)},1000)
         }
+        // check if data in cache
+        localforage.keys().then(function(x){
+            if(x.filter(function(xi){return xi=="dsrip.SBU_sparcs.tab"}).length==1){
+                dsrip.SBU_sparcs.docs=[];
+            }  
+        })
     }
 }
 
@@ -195,7 +213,123 @@ dsrip.pushDocs2Tab=function(docs,tab){ // extract JSON documents from x to y by 
 //localforage.getItem('xx').then(function(x){xx=x;console.log('done xx')});localforage.getItem('x').then(function(xi){x=xi;console.log('done x')})
 
 
+dsrip.doSPARC=function(id){ // displaying SPARC analysis results
+    var div = document.getElementById(id)
+    div.innerHTML='<table id="SPARCStable"><tr><td id="age_group" style="vertical-align:top"><h4>Age Group</h4></td><td>...</td></tr><tr><td id="Facility_Name" style="vertical-align:top"><h4>Facility Name</h4></td><td>...</td></tr></table>'
+    C = {}, D={}, G={}, U={}, R={}
 
+    var cf = crossfilter(dsrip.SBU_sparcs.docs)
+
+    var createPieChart=function(parm,cf,funColor,width,height){
+		if(!width){width=250}
+		if(!height){height=220}
+		C[parm]=dc.pieChart('#'+parm)
+		D[parm]=cf.dimension(function(d,i){
+    		return d[parm]
+    	})
+    	R[parm]={}
+    	jmat.unique(dsrip.SBU_sparcs.tab[parm]).map(function(p){
+    		R[parm][p]=0
+    	})
+
+    	G[parm]=D[parm].group().reduce(
+        	// reduce in
+			function(p,v){
+		    	R[parm][v[parm]]+=1
+		    	return R[parm][v[parm]]			
+			},
+			// reduce out
+			function(p,v){
+				R[parm][v[parm]]-=1
+		    	//nurseInfo.dt.patientsSelected[v.i]=false
+		    	return R[parm][v[parm]]
+			},
+			// ini
+			function(){
+				return 0
+			}
+    	)
+
+    	C[parm]
+    		.width(width)
+			.height(height)
+			.radius(100)
+			.innerRadius(30)
+			.dimension(D[parm])
+			.group(G[parm])
+			
+		if(funColor){
+			C[parm]
+				.colors(d3.scale.linear().domain(colorMap.domain).range(colorMap.range))
+				.colorAccessor(funColor)
+		}
+	}
+
+	var createRowChart=function(parm,cf,funColor,width,height,funOrder){
+		C[parm]=dc.rowChart('#'+parm.replace(/ /g,'_').replace(/\W/g,''))
+		D[parm]=cf.dimension(function(d){
+    		return d[parm]
+    	})
+    	R[parm]={}
+    	jmat.unique(dsrip.SBU_sparcs.tab[parm]).map(function(p){
+    		R[parm][p]=0
+    	})
+    	G[parm]=D[parm].group().reduce(
+        	// reduce in
+			function(p,v){
+				R[parm][v[parm]]+=1
+		    	return R[parm][v[parm]]			
+			},
+			// reduce out
+			function(p,v){
+				R[parm][v[parm]]-=1
+				return R[parm][v[parm]]
+			},
+			// ini
+			function(p,v){
+				return 0
+			}
+    	)
+
+    	if(!width){width=500}
+    	if(!height){
+    		height=50+G[parm].all().length*width/40
+    		//console.log(parm,width,height)
+    	}
+    	else{height=250}
+		C[parm]
+    		.width(width)
+			.height(height)
+			.elasticX(true)
+			.dimension(D[parm])
+			.group(G[parm])
+			.title(function(d){return d[parm]});
+		if(funColor){
+			C[parm]
+				.colors(d3.scale.linear().domain(colorMap.domain).range(colorMap.range))
+				.colorAccessor(funColor)
+		}
+		if(funOrder){
+			C[parm]
+				.ordering(funOrder)
+		}
+	}
+
+	createPieChart("age_group",cf)
+	createRowChart("Facility Name",cf)
+
+
+
+	dc.renderAll();
+    $('.dc-chart g text').css('fill','black');
+    $('#SPARCStable td').css({color:"navy"})
+
+
+
+    4
+
+
+}
 
 
 
